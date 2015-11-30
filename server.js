891a7989ff4d399fe3b83389ce6a1747c97903d5
Clayton Herendeen <kaos777s8@yahoo.com>
2015-11-30 17:28:06 -0500

    rearranged code

--- server.js
+++ server.js
@@ -24,53 +24,12 @@ app.set('view engine', 'jade');
 
 // ROUTES FOR OUR API
 // =============================================================================
-var router = express.Router();              // get an instance of the express Router
 
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
-app.use('/api', router);
-
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });
-});
-
-// send signup page to client
-router.get('/signup', function(req, res){
-  res.render('signup', {title:'signup'});
-});
-
-// get data from client
-router.post('/signup', function(req, res){
-  var username = req.body.username;
-
-  var user = new User({
-    username : username
-  });
-
-  user.save(function (err, data) {
-    if (err) console.log(err);
-    else console.log('Saved : ', data );
-  });
-
-  res.send('success: ' + username);
-});
-
-// for users page
-router.get('/users', function(req, res){
-  User.find({}, function(err, users){
-    var resUsers = [];
-    users.forEach(function(user) {
-      resUsers.push(user.username);
-    });
-    res.render('users', {title:'users', users:resUsers});
-  });
-});
 
 // more routes for our API will happen here
 
 // START THE SERVER
 // =============================================================================
-var port = 8080
+var port = 8080;
 app.listen(port);
 console.log('Magic happens on port ' + port);
